55332d21a1db69ed283d084e6cd87716ed7b2de2
case <case@casesandberg.com>
2016-10-28 14:12:46 -0700

    Cleanup Middleware File

--- redux-nylas-middleware/index.js
+++ redux-nylas-middleware/index.js
@@ -9,10 +9,10 @@ export const SET_TOKEN = 'NYLAS/SET_TOKEN'
 export const CLEAR_TOKEN = 'NYLAS/CLEAR_TOKEN'
 
 export default (opts = {}) => () => next => (action) => {
-  const options = { ...defaults, ...opts }
   const apiAction = action[NYLAS_API]
   if (typeof apiAction === 'undefined') { return next(action) }
 
+  const options = { ...defaults, ...opts }
   const token = localStorage.getItem(tokenKey) || null
   const { endpoint, types, method, body } = apiAction
   const [REQUEST, SUCCESS, ERROR] = types
@@ -22,11 +22,10 @@ export default (opts = {}) => () => next => (action) => {
     method: method || options.method,
     headers: { 'Authorization': `Bearer ${ token }` },
     body: body ? JSON.stringify(body) : null,
-  }).then(data => data.json()).then((response) => {
-    next({ type: SUCCESS, response })
-  }).catch((err) => {
-    next({ type: ERROR, error: err.message || 'There was an error.', err })
   })
+  .then(data => data.json())
+  .then(response => next({ type: SUCCESS, response }))
+  .catch(error => next({ type: ERROR, error }))
 }
 
 export const actions = {
