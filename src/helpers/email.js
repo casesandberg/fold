7f2a44ea88c3d058157c5e44e204b6bf3de711fa
case <case@casesandberg.com>
2016-10-29 20:15:25 -0700

    `formatEmail`

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -7,9 +7,7 @@ function parseHTMLString(string) {
   return parser.parseFromString(string, 'text/html')
 }
 
-export const chopQuotedText = (string) => {
-  const doc = parseHTMLString(string)
-
+function chopQuotedText(doc) {
   const elementsToRemove = [
     ...doc.querySelectorAll('blockquote'),
     ...doc.querySelectorAll('.gmail_quote'),
@@ -18,5 +16,45 @@ export const chopQuotedText = (string) => {
   _.each(elementsToRemove, (quote) => {
     quote.parentNode && quote.parentNode.removeChild(quote)
   })
+}
+
+function resizeLargeTables(doc) {
+  const elementsToResize = [
+    ...doc.querySelectorAll('table'),
+    ...doc.querySelectorAll('div'),
+    ...doc.querySelectorAll('td'),
+  ]
+
+  _.map(elementsToResize, (el) => {
+    if (el.width > 599 || parseInt(el.style.width, 10) > 599) {
+      console.log('LARGE RESIZE') // eslint-disable-line no-console
+      el.style.width = 'auto' // eslint-disable-line no-param-reassign
+      el.style.maxWidth = '600px' // eslint-disable-line no-param-reassign
+    }
+  })
+}
+
+function resizeImages(doc) {
+  const elementsToResize = [
+    ...doc.querySelectorAll('img'),
+  ]
+  _.map(elementsToResize, (el) => {
+    if (el.width === 0 || el.width > 599) {
+      console.log('IMAGE RESIZE') // eslint-disable-line no-console
+      el.style.maxWidth = '100%' // eslint-disable-line no-param-reassign
+      el.style.height = 'auto' // eslint-disable-line no-param-reassign
+    }
+  })
+}
+
+export const formatEmail = (string) => { // eslint-disable-line
+  const doc = parseHTMLString(string)
+
+  chopQuotedText(doc)
+  resizeLargeTables(doc)
+
+  // experimental (messes with complex newsletters)
+  resizeImages(doc)
+
   return doc.body.innerHTML.replace(/(<br><br>On\s.*\swrote:<br>)/, '')
 }
