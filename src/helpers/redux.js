888227761af1457e3a0195cc93959cb5ff6428b4
case <case@casesandberg.com>
2016-10-31 16:22:47 -0700

    cleanUp scopeStateToSelectors

--- src/helpers/redux.js
+++ src/helpers/redux.js
@@ -1,12 +1,10 @@
 import _ from 'lodash'
 
 export const scopeStateToSelectors = (selectorsMap) => {
-  return _.reduce(selectorsMap, (result, selectors, scope) => {
-    _.each(selectors, (selector, selectorName) => {
-      result[selectorName] = (state, ...rest) => { // eslint-disable-line no-param-reassign
-        return selector(state[scope], ...rest)
-      }
-    })
-    return result
-  }, {})
+  return _.reduce(selectorsMap, (allScopedSelectors, selectors, scope) => (
+    _.reduce(selectors, (scopedSelectors, selector, selectorName) => ({
+      ...scopedSelectors,
+      [selectorName]: (state, ...rest) => selector(state[scope], ...rest),
+    }), allScopedSelectors)
+  ), {})
 }
