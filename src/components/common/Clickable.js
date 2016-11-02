a4406f399f5d1af871d0e951657d9cfb59176785
case <case@casesandberg.com>
2016-11-02 16:30:53 -0700

    Clickable - cursor: pointer

--- src/components/common/Clickable.js
+++ src/components/common/Clickable.js
@@ -3,7 +3,15 @@ import { Platform, TouchableWithoutFeedback } from 'react-native'
 
 export const Clickable = (props) => {
   const Component = Platform.OS === 'web' ? 'div' : TouchableWithoutFeedback
-  return <Component { ...props } onPress={ props.onClick }>{ props.children }</Component>
+  return (
+    <Component
+      { ...props }
+      style={{ ...props.style, cursor: 'pointer' }}
+      onPress={ props.onClick }
+    >
+      { props.children }
+    </Component>
+  )
 }
 
 export default Clickable
