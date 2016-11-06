becef5765b06ffd30d2e9c0be916fccab055c975
case <case@casesandberg.com>
2016-11-06 00:29:33 -0700

    Put Custom Modules with Other Custom Modules

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -17,8 +17,8 @@ export default validate({
 
   resolve: {
     alias: {
-      'react-native': path.resolve(__dirname, './fake.react-native.js'),
-      'react-native-svg': path.resolve(__dirname, './fake.react-native-svg.js'),
+      'react-native': path.resolve(__dirname, './modules/fake.react-native.js'),
+      'react-native-svg': path.resolve(__dirname, './modules/fake.react-native-svg.js'),
       'react-universal': path.resolve(__dirname, './modules/react-universal/index.js'),
       'react-universal-auth': path.resolve(__dirname, './modules/react-universal-auth/index.js'),
     },
